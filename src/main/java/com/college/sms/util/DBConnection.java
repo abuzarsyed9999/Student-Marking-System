7bac65e79a2225b3b1757dcad2173a2f835be7e3
Syed Abuzar <abuzarcda@gmail.com>
2026-03-05 14:50:00 +0530

    Update DBConnection.java

--- src/main/java/com/college/sms/util/DBConnection.java
+++ src/main/java/com/college/sms/util/DBConnection.java
@@ -8,8 +8,8 @@ public class DBConnection {
 
     private static final String URL =
             "jdbc:mysql://localhost:3306/student_marks_system?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "#Abuzar99";
+    private static final String USER = "username";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() {
         Connection connection = null;
